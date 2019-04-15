ce0324cc8c983ec54892b3fc5c8a4cb90c20b5b7
Tom Na Thalang <nathalang_t@hotmail.ca>
2019-04-15 00:09:53 -0400

    Updates

---
+++ precache-manifest.77bcab482ef089b6acde0c278181d06f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eeb5b8de474ee2bd070",
+    "url": "tnathalang.github.io/portfolio/static/css/main.1fdb640d.chunk.css"
+  },
+  {
+    "revision": "8eeb5b8de474ee2bd070",
+    "url": "tnathalang.github.io/portfolio/static/js/main.fafddf82.chunk.js"
+  },
+  {
+    "revision": "7882c7163859101998aa",
+    "url": "tnathalang.github.io/portfolio/static/js/runtime~main.7af8eded.js"
+  },
+  {
+    "revision": "b8f155f7e34da8febef1",
+    "url": "tnathalang.github.io/portfolio/static/css/2.540052e0.chunk.css"
+  },
+  {
+    "revision": "b8f155f7e34da8febef1",
+    "url": "tnathalang.github.io/portfolio/static/js/2.e4e2615f.chunk.js"
+  },
+  {
+    "revision": "4d60e533cd5186440c5e3e78601e9d57",
+    "url": "tnathalang.github.io/portfolio/static/media/IMG_0382.4d60e533.jpg"
+  },
+  {
+    "revision": "419a764835e199cb31d0727e7df394a9",
+    "url": "tnathalang.github.io/portfolio/static/media/sakura.419a7648.jpeg"
+  },
+  {
+    "revision": "b48a2fe1cda1ab36a575c5d4fc30d63c",
+    "url": "tnathalang.github.io/portfolio/static/media/AbeKislevitz_NightLake_RMBP.b48a2fe1.jpg"
+  },
+  {
+    "revision": "b1ea4e917052530babba2146d0afa0b6",
+    "url": "tnathalang.github.io/portfolio/static/media/tom.b1ea4e91.jpg"
+  },
+  {
+    "revision": "ccbb437bcec8034d70b2946befd80b52",
+    "url": "tnathalang.github.io/portfolio/static/media/chatty.ccbb437b.png"
+  },
+  {
+    "revision": "39261aec05160bd0c64dd5084ed2b69d",
+    "url": "tnathalang.github.io/portfolio/static/media/pearup.39261aec.png"
+  },
+  {
+    "revision": "ee96c250ea563ddf86ed1501a6704ee6",
+    "url": "tnathalang.github.io/portfolio/static/media/jungle.ee96c250.png"
+  },
+  {
+    "revision": "5cfe3a503422a8c71eb2345e922c17ea",
+    "url": "tnathalang.github.io/portfolio/static/media/tweeter.5cfe3a50.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "tnathalang.github.io/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "tnathalang.github.io/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "tnathalang.github.io/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "tnathalang.github.io/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "tnathalang.github.io/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "29e6c5f3459e16accb2087d2260fbc40",
+    "url": "tnathalang.github.io/portfolio/index.html"
+  }
+];

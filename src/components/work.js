de4b38f8ad0364717e1c1902eeab6f78c3766335
Tom Na Thalang <nathalang_t@hotmail.ca>
2019-04-14 23:13:58 -0400

    basic portfolio section

--- src/components/work.js
+++ src/components/work.js
@@ -3,6 +3,7 @@ import Chatty from '../images/chatty.png'
 import pearup from '../images/pearup.png'
 import jungle from '../images/jungle.png'
 import tweeter from '../images/tweeter.png'
+import { Container, Row, Col } from 'reactstrap';
 
 
 
@@ -13,36 +14,46 @@ class Work extends Component {
     render() {
         return (
             <div href="#portfolio">
-                <div >
+                <div className="portfolio">
                     <div className="bgimg-2 w3-display-container w3-opacity-min">
                         <div className="w3-display-middle">
                             <span className="w3-xxlarge w3-text-white w3-wide">PORTFOLIO</span>
                         </div>
                     </div>
 
-                    <div className="w3-content w3-container w3-padding-64" id="portfolio">
-                        <h3 className="w3-center">MY WORK</h3>
-                        <p className="w3-center"><em>Here are some of my latest works I did.<br /> Click on the images to make them bigger</em></p><br />
+                    <div className="w3-content w3-container w3-padding-16" id="portfolio">
+                        <h3 className="w3-center">MY WORKS</h3>
+                        <p className="w3-center"><em>Here are some of my latest works I did.<br /> Hover to see the tech stacks</em></p><br />
                     </div>
                 </div>
-                <div class="grid-container">
-                    <div className="col-md-6">
-                        <img src={pearup} style={{ width: '100%' }} onclick="onClick(this)" className="hover" alt="Pear-UP!" />
-                        <p className="text-work">some demo text</p>
-                    </div>
-                    <div className="col-md-6">
-                        <img src={jungle} style={{ width: '100%' }} onclick="onClick(this)" className="hover" alt="Jungle" />
-                        <p className="text-work">some demo text</p>
-                    </div>
-                    <div className="col-md-6">
-                        <img src={Chatty} style={{ width: '100%' }} onclick=" onClick(this)" className="hover" alt="Chatty" />
-                        <p className="text-work">some demo text</p>
-                    </div>
-                    <div className="col-md-6">
-                        <img src={tweeter} style={{ width: '100%' }} onclick="onClick(this)" className="hover" alt="Tweeter" />
-                        <p className="text-work">some demo text</p>
-                    </div>
-                </div>
+                <Container>
+                    <Row>
+                        <Col xs="6">
+                            <div class="container">
+                                <img src={pearup} className="hover" alt="Pear-UP!" />
+                                <a className="tech-stack" href="https://github.com/tnathalang/Pear-Up-">Made using React, Bootstrap, Rails and Postgres</a>
+                                <p className="text-work">Matching base on common interest and asking someone to do an activity within a coworker space on the fly type of app</p>
+                            </div>
+                            <div>
+                                <img src={jungle} className="hover" alt="Jungle" />
+                                <a className="tech-stack" href="https://github.com/tnathalang/jungle-rails">Made using Ruby on Rails, Bootstrap</a>
+                                <p className="text-work">An e-commerce Rails app, have review system and purchase system with Stripes</p>
+                            </div>
+                        </Col>
+                        <Col xs="6">
+                            <div>
+                                <img src={Chatty} className="hover" alt="Chatty" />
+                                <a className="tech-stack" href="https://github.com/tnathalang/chatty">Made using React, CSS 3</a>
+                                <p className="text-work">A minimal chatting app built with React, support multiple users</p>
+                            </div>
+                            <div>
+                                <img src={tweeter} className="hover" alt="Tweeter" />
+                                <a className="tech-stack" href="https://github.com/tnathalang/tweeter">Made using JQuery, AJAX, Express, Node.js and CSS</a>
+                                <p className="text-work">A twitter clone project to help learn front-end and css practice</p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }

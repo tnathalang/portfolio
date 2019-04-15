b4161a572f53926e3412d11d9e4c914cc3994f3c
Tom Na Thalang <nathalang_t@hotmail.ca>
2019-04-14 23:25:02 -0400

    add href to navbar

--- src/components/header.js
+++ src/components/header.js
@@ -32,18 +32,20 @@ class Header extends Component {
 
             <div className="w3-top">
                 <div className="w3-bar" id="myNavbar">
-                    <a className="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
+                    <a className="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" title="Toggle Navigation Menu">
                         <i className="fa fa-bars" />
                     </a>
                     <a href="#about" className="w3-bar-item w3-button w3-hide-small"><i className="fa fa-user" /> ABOUT</a>
                     <a href="#portfolio" className="w3-bar-item w3-button w3-hide-small"><i className="fa fa-th" /> PORTFOLIO</a>
                     <a href="#contact" className="w3-bar-item w3-button w3-hide-small"><i className="fa fa-envelope" /> CONTACT</a>
+                    <a href="https://www.linkedin.com/in/tom-na-thalang" className="w3-bar-item w3-button w3-hide-small"><i class="fab fa-linkedin-in"></i> LINKEDIN </a>
+                    <a href="https://www.instagram.com/itsdrunksushi/" className="w3-bar-item w3-button w3-hide-small"><i class="fab fa-instagram"></i> INSTAGRAM </a>
                 </div>
                 {/* Navbar on small screens */}
                 <div id="navDemo" className="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-                    <a href="#about" className="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
-                    <a href="#portfolio" className="w3-bar-item w3-button" onclick="toggleFunction()">PORTFOLIO</a>
-                    <a href="#contact" className="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
+                    <a href="#about" className="w3-bar-item w3-button" >ABOUT</a>
+                    <a href="#portfolio" className="w3-bar-item w3-button" >PORTFOLIO</a>
+                    <a href="#contact" className="w3-bar-item w3-button" >CONTACT</a>
                 </div>
             </div>
         );

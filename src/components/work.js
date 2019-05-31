fec465fc7e63681e80063685b2381fdca484ace4
Tom Na Thalang <nathalang_t@hotmail.ca>
2019-05-31 13:38:24 -0400

    css in work section

--- src/components/work.js
+++ src/components/work.js
@@ -50,151 +50,169 @@ function MediaCard() {
           <span className="w3-xxlarge w3-text-white w3-wide">PORTFOLIO</span>
         </div>
       </div>
-      <div className="w3-content w3-container w3-padding-16" id="portfolio">
-        <h3 className="w3-center">MY WORKS</h3>
-        <p className="w3-center">
-          <em>Here are some of my latest works I did.</em>
-        </p>
-        <br />
-      </div>
-      <div classname="containerGrid">
-        <div className="box">
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={pearup}
-                title="Social Media for introverts"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Pear-UP!
-                </Typography>
-                <Typography>
-                  Matching users base on common interests and for scheduling a
-                  meetup with someone to do an activity within a coworker space
-                  on the fly type of app
-                </Typography>
-                <Typography variant="body3" color="textSecondary" component="p">
-                  Made using React on Rails, with Postgres DB, Bootstrap and
-                  SASS for styling
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                href="https://github.com/tnathalang/Pear-Up-"
-                target="_blank"
-              >
-                Github Link
-              </Button>
-            </CardActions>
-          </Card>
+      <div id="portfolio">
+        <div>
+          <h3 className="w3-center">MY WORKS</h3>
+          <p className="w3-center">
+            <em>Here are some of my latest works I did.</em>
+          </p>
+          <br />
         </div>
-        <div className="box">
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={jungle}
-                title="eCommerce Plateform"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Jungle
-                </Typography>
-                <Typography>
-                  An e-commerce Ruby on Rails app, offers a review system and
-                  purchase options with Stripes. Required Users to have an
-                  account
-                </Typography>
-                <Typography variant="body3" color="textSecondary" component="p">
-                  Made using React on Rails, with Postgres DB, Bootstrap and
-                  SASS for styling
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                href="https://github.com/tnathalang/jungle-rails"
-                target="_blank"
-              >
-                Github Link
-              </Button>
-            </CardActions>
-          </Card>
-        </div>
-        <div className="box">
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={Chatty}
-                title="Chatting app"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Chatty
-                </Typography>
-                <Typography>
-                  A minimal chatting app built with React, support multiple
-                  users. You can also change colors of your name! no log ins
-                  needed
-                </Typography>
-                <Typography variant="body3" color="textSecondary" component="p">
-                  Made using React and SASS
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                href="https://github.com/tnathalang/chatty"
-                target="_blank"
-              >
-                Github Link
-              </Button>
-            </CardActions>
-          </Card>
-        </div>
-        <div className="box">
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={tweeter}
-                title="A Twitter clone"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Tweeter
-                </Typography>
-                <Typography>
-                  A twitter clone project to help learn front-end without
-                  frameworks and css practice, flexbox etc.
-                </Typography>
-                <Typography variant="body3" color="textSecondary" component="p">
-                  Made with Ruby on Rails, with Postgres DB, Bootstrap and CSS 3
-                  for styling
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                href="https://github.com/tnathalang/tweeter"
-                target="_blank"
-              >
-                Github Link
-              </Button>
-            </CardActions>
-          </Card>
+        <div classname="grid-container">
+          <div>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={pearup}
+                  title="Social Media for introverts"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Pear-UP!
+                  </Typography>
+                  <Typography>
+                    Matching users base on common interests and for scheduling a
+                    meetup with someone to do an activity within a coworker
+                    space on the fly type of app
+                  </Typography>
+                  <Typography
+                    variant="body3"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Made using React on Rails, with Postgres DB, Bootstrap and
+                    SASS for styling
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  size="small"
+                  color="primary"
+                  href="https://github.com/tnathalang/Pear-Up-"
+                  target="_blank"
+                >
+                  Github Link
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+          <div className="box">
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={jungle}
+                  title="eCommerce Plateform"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Jungle
+                  </Typography>
+                  <Typography>
+                    An e-commerce Ruby on Rails app, offers a review system and
+                    purchase options with Stripes. Required Users to have an
+                    account
+                  </Typography>
+                  <Typography
+                    variant="body3"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Made using React on Rails, with Postgres DB, Bootstrap and
+                    SASS for styling
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  size="small"
+                  color="primary"
+                  href="https://github.com/tnathalang/jungle-rails"
+                  target="_blank"
+                >
+                  Github Link
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+          <div className="box">
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={Chatty}
+                  title="Chatting app"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Chatty
+                  </Typography>
+                  <Typography>
+                    A minimal chatting app built with React, support multiple
+                    users. You can also change colors of your name! no log ins
+                    needed
+                  </Typography>
+                  <Typography
+                    variant="body3"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Made using React and SASS
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  size="small"
+                  color="primary"
+                  href="https://github.com/tnathalang/chatty"
+                  target="_blank"
+                >
+                  Github Link
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+          <div className="box">
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={tweeter}
+                  title="A Twitter clone"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Tweeter
+                  </Typography>
+                  <Typography>
+                    A twitter clone project to help learn front-end without
+                    frameworks and css practice, flexbox etc.
+                  </Typography>
+                  <Typography
+                    variant="body3"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Made with Ruby on Rails, with Postgres DB, Bootstrap and CSS
+                    3 for styling
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  size="small"
+                  color="primary"
+                  href="https://github.com/tnathalang/tweeter"
+                  target="_blank"
+                >
+                  Github Link
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
